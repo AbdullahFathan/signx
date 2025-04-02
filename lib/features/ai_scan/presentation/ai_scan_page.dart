f61038d14cf7f9d9307dae52016940af8a51e2e9
AbdullahFathan <abdullahfathan013@gmail.com>
2025-04-02 15:32:24 +0700

    feat: add camera to ai scan page

--- lib/features/ai_scan/presentation/ai_scan_page.dart
+++ lib/features/ai_scan/presentation/ai_scan_page.dart
@@ -1,10 +1,50 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:signx/config/theme/resources/app_color.dart';
+import 'package:signx/main.dart';
 import 'package:signx/widgets/primary_appbar.dart';
 
-class AiScanPage extends StatelessWidget {
+class AiScanPage extends StatefulWidget {
   static const String route = "/ai-scan";
   const AiScanPage({super.key});
+
+  @override
+  State<AiScanPage> createState() => _AiScanPageState();
+}
+
+class _AiScanPageState extends State<AiScanPage> {
+  late CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(cameras[0], ResolutionPreset.max);
+    controller
+        .initialize()
+        .then((_) {
+          if (!mounted) {
+            return;
+          }
+          setState(() {});
+        })
+        .catchError((Object e) {
+          if (e is CameraException) {
+            switch (e.code) {
+              case 'CameraAccessDenied':
+                // Handle access errors here.
+                break;
+              default:
+                // Handle other errors here.
+                break;
+            }
+          }
+        });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +66,7 @@ class AiScanPage extends StatelessWidget {
               child: Container(
                 margin: const EdgeInsets.symmetric(vertical: 24, horizontal: 8),
                 width: double.infinity,
-                color: AppColors.primaryVariantColror,
+                child: childCamera(),
               ),
             ),
             Text(
@@ -40,5 +80,17 @@ class AiScanPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Widget childCamera() {
+    if (!controller.value.isInitialized) {
+      return Center(
+        child: Text(
+          "Kamera Tidak Bisa Diakses",
+          style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontSize: 16),
+        ),
+      );
+    }
+    return CameraPreview(controller);
   }
 }
