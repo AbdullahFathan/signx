8cfd640f84794b63eeaba439edbbd03031d72321
AbdullahFathan <abdullahfathan013@gmail.com>
2025-04-03 15:17:35 +0700

    feat: setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:camera/camera.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:signx/config/routes/app_route.dart';
 import 'package:signx/config/theme/app_theme.dart';
+import 'package:signx/firebase_options.dart';
 
 late List<CameraDescription> cameras;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   cameras = await availableCameras();
-  runApp(const MainApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(ProviderScope(child: const MainApp()));
 }
 
 class MainApp extends StatelessWidget {
